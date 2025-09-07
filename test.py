e63361ff2a42c32c508945c92effbf28101bd873
hiennguyendang <hien.nguyendang@hcmut.edu.vn>
2025-09-07 21:31:25 +0700

    initial commit

--- test.py
+++
@@ -1,7 +0,0 @@
-# matplotlib inline
-import matplotlib.pyplot as plt
-
-plt.figure(figsize=(4, 2))  # Chiều rộng 4 inch, chiều cao 2 inch
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.title("Thu nhỏ cửa sổ Matplotlib trên Jupyter")
-plt.show()
